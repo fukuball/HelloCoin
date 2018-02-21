e565788156b833135d536cf519d6c61e761409b7
Fukuball Lin <fukuball@gmail.com>
2018-02-21 16:45:52 +0800

    Debugging

--- truffle.js
+++ truffle.js
@@ -10,7 +10,7 @@ module.exports = {
       host: "localhost",
       port: 8546,
       from: "0x6898a60bb7a38f5b70cb6d18930177d1d5b9eaa6",
-      gas: 25200,
+      gas: 84000,
       gasPrice: 21000000000,
       network_id: "development" // Match any network id
     },
@@ -18,7 +18,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       from: "0x6898a60bb7a38f5b70cb6d18930177d1d5b9eaa6",
-      gas: 25200,
+      gas: 84000,
       gasPrice: 21000000000,
       network_id: "mainnet" // Match any network id
     }
