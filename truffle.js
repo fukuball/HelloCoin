b180469477b67f406968ac2799f96b541d158dfc
Fukuball Lin <fukuball@gmail.com>
2018-02-21 16:01:55 +0800

    Debugging

--- truffle.js
+++ truffle.js
@@ -2,16 +2,11 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    development: {
-      host: "localhost",
-      port: 8546,
-      network_id: "development" // Match any network id
-    },
     mainnet: {
       host: "localhost",
       port: 8545,
       from: "0x6898a60bb7a38f5b70cb6d18930177d1d5b9eaa6",
-      gas: 2000000,
+      gas: 4700000,
       network_id: "mainnet" // Match any network id
     }
   }
