3e8ace1f665f2a8f71ea027c581ffe6f64a4b8d8
Fukuball Lin <fukuball@gmail.com>
2018-02-21 15:58:29 +0800

    Debugging

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations, {
+    gas: 2000000,
+    from: "0x6898a60bb7a38f5b70cb6d18930177d1d5b9eaa6"
+  });
 };
