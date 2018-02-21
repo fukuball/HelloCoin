25ee03d22f9c570dcd5eaea9e6852e31a317d585
Fukuball Lin <fukuball@gmail.com>
2018-02-21 16:12:55 +0800

    Debugging

--- migrations/2_deploy_hellocoin.js
+++ migrations/2_deploy_hellocoin.js
@@ -2,7 +2,7 @@ var HelloCoin = artifacts.require("HelloCoin");
 
 module.exports = function(deployer) {
   deployer.deploy(HelloCoin, {
-    gas: 2000000,
+    gas: 8000029,
     from: "0x6898a60bb7a38f5b70cb6d18930177d1d5b9eaa6"
   });
 };
--- truffle.js
+++ truffle.js
@@ -1,12 +1,23 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  rpc: {
+    host: "localhost",
+    port: 8545
+  },
   networks: {
+    development: {
+      host: "localhost",
+      port: 8546,
+      from: "0x6898a60bb7a38f5b70cb6d18930177d1d5b9eaa6",
+      gas: 8000029,
+      network_id: "development" // Match any network id
+    },
     mainnet: {
       host: "localhost",
       port: 8545,
       from: "0x6898a60bb7a38f5b70cb6d18930177d1d5b9eaa6",
-      gas: 4700000,
+      gas: 8000029,
       network_id: "mainnet" // Match any network id
     }
   }
