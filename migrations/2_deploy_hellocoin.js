255200243384276dacdddbd7556aa4b48a7db7a4
Fukuball Lin <fukuball@gmail.com>
2018-02-21 15:56:07 +0800

    deploy

--- migrations/2_deploy_hellocoin.js
+++ migrations/2_deploy_hellocoin.js
@@ -1,5 +1,8 @@
 var HelloCoin = artifacts.require("HelloCoin");
 
 module.exports = function(deployer) {
-  deployer.deploy(HelloCoin);
+  deployer.deploy(HelloCoin, {
+    gas: 2000000,
+    from: "0x6898a60bb7a38f5b70cb6d18930177d1d5b9eaa6"
+  });
 };
